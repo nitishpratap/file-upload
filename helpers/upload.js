a6e71fb58f482dacd7264eb20e6a6c591d393e2e
nitish-yadav-robomq <nitish.yadav@robomq.io>
2025-05-04 17:40:04 +0530

    uploaded on local system

--- helpers/upload.js
+++ helpers/upload.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+
+
+
 async function upload(req) {
     const files = req.files;
     // console.log(files);
@@ -16,20 +19,22 @@ async function upload(req) {
         }else {
             //save to upload
             const targetPath = path.join(__dirname, `../upload/${fileName}`);
+
             try {
                 fs.writeFile(targetPath,file.buffer,function (err) {
                     if (err) throw err;
-                    console.log("Successfully uploaded ::::::::",file.filename);
+                    console.log("Successfully uploaded ::::::::",file.originalname);
                 });
                 return {
                     status: 'success',
+                    fileName : file.originalname,
                     message : "Saved to local system"
                 }
             }catch(err) {
                 return {
-                    status: 'success',
-                    fileName : file.fileName,
-                    message : "Saved to local system"
+                    status: 'Error',
+                    fileName : file.originalname,
+                    message : "Failed to upload file"
                 }
             }
         }
